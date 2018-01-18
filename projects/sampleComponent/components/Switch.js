037b178bcb5fb8265df9f05b6057feca4893f64e
Nick Hendricks <moabs81@hotmail.com>
2018-01-18 15:51:39 -0500

    updated a few things

--- projects/sampleComponent/components/Switch.js
+++ projects/sampleComponent/components/Switch.js
@@ -1,8 +1,59 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 
-const SampleThing = () => <div className = 'sampleApp'>Hi, let's try breaking the cache, for real this time...</div>
+import PropTypes from 'prop-types';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
+
+const initialState = {
+    onOff: false
+};
+
+const setOnOff = onOff => {
+    return { type: 'SET_ON_OFF', onOff };
+};
+
+const switchReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_ON_OFF':
+            return Object.assign(
+                {},
+                state,
+                { onOff: action.onOff }
+            );
+        default:
+            return state;
+    };
+};
+
+const store = createStore(switchReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const SwitchVisualComponent = ({ onOrOff, onOffEvent }) => <label><input type='checkbox' /> hi! </label>
+
+const SwitchComponent = () => {
+    <div className='switchMain'>
+        <SwitchContainerComponent />
+    </div>
+};
+
+const mapSwitchStateToProps = state => {
+    return {
+        onOff: switchReducer(state.onOff, setOnOff)
+    };
+};
+const mapSwitchDispatchToProps = dispatch => {
+    return {
+        onOffEvent: () => {
+            dispatch(setOnOff());
+        }
+    };
+};
+
+const SwitchContainerComponent = connect(mapSwitchStateToProps, mapSwitchDispatchToProps)(SwitchVisualComponent);
 
 ReactDom.render(
-    <SampleThing />, document.getElementById('contentContainer')
+    <Provider store={store}>
+        <SwitchComponent />
+    </Provider>,
+    document.getElementById('contentContainer')
 );
--- workbench/workbench.js
+++ workbench/workbench.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-import {Provider, connect} from 'react-redux';
-import {createStore} from 'redux';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
 import backgroundImage from './chalkboard.jpeg';
 import './workbench.less';
 
@@ -13,39 +13,39 @@ const initialState = {
     isIE: false
 };
 //Action
-const getScrollPos = function(position){
-    return {type:'GET_POSITION',position};
+const getScrollPos = function (position) {
+    return { type: 'GET_POSITION', position };
 };
-const isBrowserIE = function(isIE){
-    return{type:'GET_BROWSER_TYPE',isIE};
+const isBrowserIE = function (isIE) {
+    return { type: 'GET_BROWSER_TYPE', isIE };
 };
 //Reducer
-const scrollPosReducer = function(state = initialState, action){
-    switch(action.type){
+const scrollPosReducer = function (state = initialState, action) {
+    switch (action.type) {
         case 'GET_POSITION':
             return Object.assign(
                 {},
                 state,
-                {position: action.position}
+                { position: action.position }
             );
         case 'GET_BROWSER_TYPE':
             return Object.assign(
                 {},
                 state,
-                {isIE: action.isIE}
+                { isIE: action.isIE }
             );
         default:
             return state;
     };
-    return state; 
+    return state;
 };
 
 //Create Store
 const store = createStore(scrollPosReducer);
 
 //Presentational Components
-export const TopMenu = ({position}) =>(
-    <div className= {position < 140? 'titleContainer titleContainerSolid' : 'titleContainer'}>
+export const TopMenu = ({ position }) => (
+    <div className={position < 140 ? 'titleContainer titleContainerSolid' : 'titleContainer'}>
         <div className='menu'>
             <div className='menuContent leftTitle'>
                 <h2>Component Demo Workbench</h2>
@@ -53,40 +53,48 @@ export const TopMenu = ({position}) =>(
         </div>
     </div>
 );
-export const BodyContent = ({scrollEvent}) => (
-    <div className='parallaxClass' id='allContentDiv' onScroll = {scrollEvent}>
-        <div className = 'parallax background'>
-            <img id='backgroundImg' src={backgroundImage}/>
+TopMenu.proptypes = {
+    position: PropTypes.number.isRequired
+};
+
+export const BodyContent = ({ scrollEvent }) => (
+    <div className='parallaxClass' id='allContentDiv' onScroll={scrollEvent}>
+        <div className='parallax background'>
+            <img id='backgroundImg' src={backgroundImage} />
         </div>
-        <div className = 'parallax foreground'>
+        <div className='parallax foreground'>
             <div className='contentContainer' id='contentContainer'></div>
         </div>
     </div>
 );
+BodyContent.propTypes = {
+    scrollEvent: PropTypes.func.isRequired
+};
+
 export const WorkBenchComponent = () => (
-    <div className = 'workBenchMain'>
+    <div className='workBenchMain'>
         <AnchoringTopContent />
         <ScrollingBottomContent />
     </div>
 );
 
 //Connect Functions
-const mapStateToProps = state => {    
+const mapStateToProps = state => {
     return {
-        position: scrollPosReducer(state.position,getScrollPos)
+        position: scrollPosReducer(state.position, getScrollPos)
     };
 };
 const mapDispatchToProps = dispatch => {
-    return{
+    return {
         scrollEvent: () => {
-            dispatch(getScrollPos(document.getElementById('contentContainer').getBoundingClientRect().top));            
+            dispatch(getScrollPos(document.getElementById('contentContainer').getBoundingClientRect().top));
         }
     };
 };
 
 //Container Components
-export const AnchoringTopContent = connect(mapStateToProps, mapDispatchToProps)(TopMenu);
-export const ScrollingBottomContent = connect(mapStateToProps,mapDispatchToProps)(BodyContent);
+export const AnchoringTopContent = connect(mapStateToProps, null)(TopMenu);
+export const ScrollingBottomContent = connect(null, mapDispatchToProps)(BodyContent);
 
 //Main
 ReactDOM.render(
